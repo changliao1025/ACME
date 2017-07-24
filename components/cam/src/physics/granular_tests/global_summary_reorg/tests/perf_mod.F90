6b792984f8800274edbc639ae5638fc13e6753a7
Hui Wan <Hui.Wan@pnnl.gov>
2017-07-24 13:10:06 -0700

    bug fixes for unit test (only)

--- components/cam/src/physics/granular_tests/global_summary_reorg/tests/perf_mod.F90
+++ components/cam/src/physics/granular_tests/global_summary_reorg/tests/perf_mod.F90
@@ -3,14 +3,16 @@ module perf_mod
 implicit none
 public
 
-subroutine t_startf(timer_name)
-  character(len=*) timer_name
-  return
-end subroutine
+contains
 
-subroutine t_stopf(timer_name)
-  character(len=*) timer_name
-  return
-end subroutine
+  subroutine t_startf(timer_name)
+    character(len=*) timer_name
+    return
+  end subroutine
+  
+  subroutine t_stopf(timer_name)
+    character(len=*) timer_name
+    return
+  end subroutine
 
 end module perf_mod
