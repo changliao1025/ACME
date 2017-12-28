8e2e4930888c4f8c8c82567f64339d15000e0bd3
Steve Goldhaber <goldy@ucar.edu>
2017-12-27 22:25:27 -0600

    Utility changes to enable new phys_grid_init
    Error out of configure unless dycore is SE.
    Make ec_active a namelist variable to not rely on empty filename string
    Rewrite elevation_classes_init to use dof from phys_grid_init

--- components/cam/src/physics/elev_classes/elev_classes.F90
+++ components/cam/src/physics/elev_classes/elev_classes.F90
@@ -11,15 +11,15 @@ module elev_classes
   save
 
   ! Public module variables
-  logical, public              :: ec_active = .false.
-  integer, public              :: max_elevation_classes = 1
+  logical, public, protected :: ec_active = .false.
+  integer, public, protected :: max_elevation_classes = 1
 
   ! Private module variables
-  character(len=SHR_KIND_CL)   :: elevation_classes_filename
+  character(len=SHR_KIND_CL) :: elevation_classes_filename
 
   ! Public interface functions
-  public elevation_classes_readnl
-  public elevation_classes_init
+  public :: elevation_classes_readnl
+  public :: elevation_classes_init
 
 CONTAINS
 
@@ -40,10 +40,12 @@ CONTAINS
     integer                          :: ierr
     integer                          :: unitn
     character(len=SHR_KIND_CL)       :: elevation_classes
+    character(len=*), parameter      :: subname = "ELEVATION_CLASSES_READNL"
 
-    namelist /ec_nl/ elevation_classes
+    namelist /ec_nl/ elevation_classes, ec_active
 
     ! Default is no elevation classes
+    ec_active = .false.
     elevation_classes = ''
 
     ! Read the namelist
@@ -54,7 +56,7 @@ CONTAINS
       if (ierr == 0) then
         read(unitn, ec_nl, iostat=ierr)
         if (ierr /= 0) then
-          call endrun('elevation_classes_readnl: ERROR reading ec_nl namelist')
+          call endrun(subname//': ERROR reading ec_nl namelist')
         end if
       end if
       close(unitn)
@@ -62,7 +64,9 @@ CONTAINS
 
       elevation_classes_filename = elevation_classes
 
-      ec_active = (len_trim(elevation_classes_filename) > 0)
+      if (ec_active .and. (len_trim(elevation_classes_filename) == 0)) then
+        call endrun(subname//': elevation_classes filename required')
+      end if
       if (ec_active) then
         write(iulog, *) 'Elevation Classes will be read from ',trim(elevation_classes_filename)
       else
@@ -79,64 +83,95 @@ CONTAINS
   !!  elevation_classes_init is called from phys_grid_init
   !!
   !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  subroutine elevation_classes_init(input_gridname, pts_per_block, numblocks, &
-    num_subgrids, subgrid_area, subgrid_elev)
-    use ncdio_atm,        only: infld
-    use cam_pio_utils,    only: cam_pio_openfile, cam_pio_closefile
-    use cam_pio_utils,    only: cam_pio_handle_error
-    use pio,              only: PIO_NOWRITE, PIO_inq_dimid, PIO_inq_dimlen
-    use pio,              only: file_desc_t
+  subroutine elevation_classes_init(ldof, num_subgrids, subgrid_area, subgrid_elev)
+    use cam_logfile,   only: iulog
+    use spmd_utils,    only: masterproc
+    use cam_pio_utils, only: cam_pio_openfile, cam_pio_closefile
+    use cam_pio_utils, only: cam_pio_handle_error
+    use pio,           only: PIO_NOWRITE, PIO_inq_dimid, PIO_inq_dimlen
+    use pio,           only: PIO_seterrorhandling, PIO_read_darray
+    use pio,           only: PIO_initdecomp, PIO_freedecomp, PIO_inq_varid
+    use pio,           only: file_desc_t, var_desc_t, io_desc_t, iosystem_desc_t
+    use pio,           only: PIO_INT, PIO_DOUBLE
+    use pio,           only: PIO_OFFSET_KIND, PIO_BCAST_ERROR
+    use cam_instance,  only: atm_id
+    use shr_pio_mod,   only: shr_pio_getiosys
 
     !! Dummy arguments
-    character(len=*),      intent(in)  :: input_gridname
-    integer,               intent(in)  :: pts_per_block
-    integer,               intent(in)  :: numblocks
-    integer,  allocatable, intent(out) :: num_subgrids(:,:)
-    real(r8), allocatable, intent(out) :: subgrid_area(:,:,:)
-    real(r8), allocatable, intent(out) :: subgrid_elev(:,:,:)
+    integer(PIO_OFFSET_KIND), intent(in)  :: ldof(:)    ! Map to file order
+    integer,     allocatable, intent(out) :: num_subgrids(:)
+    real(r8),    allocatable, intent(out) :: subgrid_area(:,:)
+    real(r8),    allocatable, intent(out) :: subgrid_elev(:,:)
 
     !! Local variables
     type(file_desc_t)                  :: fh_ec
+    integer                            :: larray_size ! Size of dest arrays
     integer                            :: ierr
     integer                            :: dimid
-    logical                            :: found
+    integer                            :: grid_len
+    integer                            :: err_handling
+    type(io_desc_t)                    :: iodesc
+    type(var_desc_t)                   :: varid
+    type(iosystem_desc_t), pointer     :: piosys
     character(len=*), parameter        :: subname = 'ELEVATION_CLASSES_INIT'
 
     call cam_pio_openfile(fh_ec, trim(elevation_classes_filename), PIO_NOWRITE)
+    ! We will handle errors for this routine
+    call pio_seterrorhandling(fh_ec, PIO_BCAST_ERROR, err_handling)
 
-    ! We need to know the maximum number of elevation classes
+    ! We need to know the maximum number of elevation classes and grid size
+    ierr = PIO_inq_dimid(fh_ec, 'grid_size', dimid)
+    call cam_pio_handle_error(ierr, subname//': Error finding dimension, grid_size')
+    ierr = PIO_inq_dimlen(fh_ec, dimid, grid_len)
     ierr = PIO_inq_dimid(fh_ec, 'MaxNoClass', dimid)
     call cam_pio_handle_error(ierr, subname//': Error finding dimension, MaxNoClass')
     ierr = PIO_inq_dimlen(fh_ec, dimid, max_elevation_classes)
-    
-    ! Read the relevant variables
+
+    larray_size = size(ldof)
     if (allocated(num_subgrids)) then
       deallocate(num_subgrids)
     end if
-    allocate(num_subgrids(pts_per_block, numblocks))
+    allocate(num_subgrids(larray_size))
     num_subgrids = 0
-    call infld('NumSubgrids', fh_ec, 'ncol', 'MaxNoClass', 1, pts_per_block,  &
-         1, numblocks, num_subgrids, found, gridname=trim(input_gridname))
+
+    ! Read the relevant variables
+    piosys => shr_pio_getiosys(atm_id)
+    call PIO_initdecomp(piosys, PIO_INT, (/ grid_len/), ldof, iodesc)
+    ierr = PIO_inq_varid(fh_ec, 'NumSubgrids', varid)
+    call cam_pio_handle_error(ierr, subname//': Error finding variable, NumSubgrids')
+    call pio_read_darray(fh_ec, varid, iodesc, num_subgrids, ierr)
+    call cam_pio_handle_error(ierr, subname//': Error reading NumSubgrids')
+    call PIO_freedecomp(fh_ec, iodesc)
 
     if (allocated(subgrid_area)) then
       deallocate(subgrid_area)
     end if
-    allocate(subgrid_area(pts_per_block, max_elevation_classes, numblocks))
+    allocate(subgrid_area(max_elevation_classes, larray_size))
     subgrid_area = 0.0_r8
-    call infld('SubgridAreaFrac', fh_ec, 'ncol', 'MaxNoClass',                &
-         1, pts_per_block, 1, max_elevation_classes, 1, numblocks,            &
-         subgrid_area, found, gridname=trim(input_gridname))
+    call PIO_initdecomp(piosys, PIO_DOUBLE, (/ max_elevation_classes, grid_len/), ldof, iodesc)
+    ierr = PIO_inq_varid(fh_ec, 'SubgridAreaFrac', varid)
+    call cam_pio_handle_error(ierr, subname//': Error finding variable, SubgridAreaFrac')
+    call pio_read_darray(fh_ec, varid, iodesc, subgrid_area, ierr)
+    call cam_pio_handle_error(ierr, subname//': Error reading SubgridAreaFrac')
 
     if (allocated(subgrid_elev)) then
       deallocate(subgrid_elev)
     end if
-    allocate(subgrid_elev(pts_per_block, max_elevation_classes, numblocks))
+    allocate(subgrid_elev(max_elevation_classes, larray_size))
     subgrid_elev = 0.0_r8
-    call infld('AveSubgridElv', fh_ec, 'ncol', 'MaxNoClass',                  &
-         1, pts_per_block, 1, max_elevation_classes, 1, numblocks,            &
-         subgrid_elev, found, gridname=trim(input_gridname))
+    ierr = PIO_inq_varid(fh_ec, 'AveSubgridElv', varid)
+    call cam_pio_handle_error(ierr, subname//': Error finding variable, AveSubgridElv')
+    call pio_read_darray(fh_ec, varid, iodesc, subgrid_elev, ierr)
+    call cam_pio_handle_error(ierr, subname//': Error reading AveSubgridElv')
+    call PIO_freedecomp(fh_ec, iodesc)
 
+    ! Back to old error handling
+    call pio_seterrorhandling(fh_ec, err_handling)
+    ! Finally, close the file
     call cam_pio_closefile(fh_ec)
+    if (masterproc) then
+      write(iulog, *) subname, ': Read elevation classes from ', trim(elevation_classes_filename)
+    end if
   end subroutine elevation_classes_init
 
 end module elev_classes
